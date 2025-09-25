b7696875ac661de28143e42c71968ee5c5ef2d24
Nissi17 <nissi200527@gmail.com>
2025-09-25 16:48:29 +0530

    Odd or Even

---
+++ ScopeDemo.java
@@ -0,0 +1,9 @@
+public class ScopeDemo {
+    public static void main(String[]args){
+        int x=10;
+        if (x>5){
+            int y=20;
+            System.out.println("x=" + x + "y=" + y);
+        }
+    }
+}
---
+++ VariableDemo.java
@@ -0,0 +1,14 @@
+public class VariableDemo {
+    public static void main(String...args) {
+        int a =10;
+        double b =2.5;
+        char ch= 'A';
+        String s = "Hello";
+        boolean c = true;
+        System.out.println("Integer: a = " + a);
+        System.out.println("Double: b = " + b);
+        System.out.println("Character: ch = " + ch);
+        System.out.println("String: s = " + s);
+        System.out.println("Boolean: c = " + c);
+    }
+}
