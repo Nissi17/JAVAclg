efd9de7416d37804e9b409bbaa6232fa075ed870
Nissi17 <nissi200527@gmail.com>
2025-09-26 14:29:35 +0530

    Array numbers

---
+++ ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arrnumbers = {10, 20, 30, 40, 50};
+        System.out.println("Array elements:");
+        for (int x : arrnumbers) {
+            System.out.print(x + " ");
+        }
+    }
+}
